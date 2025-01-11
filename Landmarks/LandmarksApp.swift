82c5367aa093c7a98e3f55d0d2367ad2b90d866d
iamageo <geovani.af4@gmail.com>
2025-01-11 00:07:14 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Geovani Amaral on 11/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
